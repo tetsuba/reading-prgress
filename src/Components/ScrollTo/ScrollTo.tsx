7fa3a2155a2364940caecddfa8f472627623e3d0
Tetsuba <tetsuba@tetsuba.com>
2023-03-12 11:55:32 +0000

    removed scroll behaviour to fix ipad scroll not working

--- src/Components/ScrollTo/ScrollTo.tsx
+++ src/Components/ScrollTo/ScrollTo.tsx
@@ -2,10 +2,6 @@ type PropTypes = {
     top: number
 }
 export default function ScrollTo(props: PropTypes) {
-    window.scrollTo({
-        top: props.top,
-        left: 0,
-        behavior: 'smooth'
-    })
+    window.scrollTo(0, props.top)
     return null
 }
