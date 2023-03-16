314db28988ec0a10138f70e93aacae8f42dee1e1
Tetsuba <tetsuba@tetsuba.com>
2023-03-16 21:44:59 +0000

    disabled dev mode for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
                 ]
             },
             devOptions: {
-                enabled: true
+                enabled: false
             }
         })
     ],
